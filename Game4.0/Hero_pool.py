5a5980d5a72b30426fdbed190ae92cc1ddf8d83d
LiuandZhou <103430601+LiuandZhou@users.noreply.github.com>
2022-12-31 18:23:04 +0800

    Delete Game4.0 directory

--- Game4.0/Hero_pool.py
+++
@@ -1,8 +0,0 @@
-# 学校：湖南第一师范学院
-# 作者：刘浩铃
-# 时间：2022/12/30 19:37
-class Hero():
-    def __init__(self):
-        self.gold_pool = ['Lulu','Zed','Yasuo','Timo']
-        self.purple_pool = ['Li Qing','Reven','EZ','Akali']
-        self.blue_pool = ['Chui Shi','Jian Mo','Gou Xion','Miss future','Jia Kesi','Hou Zi']
